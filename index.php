393631adf353686ab6c550f5b3b6434ca6080821
Rogelio Garcia <rogelio.garcia@west-mec.org>
2016-04-27 15:22:02 -0700

    + game screenshots for home page

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 
 
@@ -14,15 +14,9 @@
 
 
 
-<div id="titlename">
-    <h1>Slither</h1>
-</div>
-
-
 </body>
 </html>
 
-=======
 <!DOCTYPE html>
 <html>
 <head>
@@ -32,13 +26,31 @@
 </head>
 <body>
 
-<ul>
+<ul style="position: fixed; top: 0">
+    <li><a href="index.php"><img src="http://cucsa.org.uk/wp-content/uploads/2015/10/Work_In_Progress.png" style="height: 100px; width: 100px;"></a> </li>
     <li><a href="action.php">Action</a></li>
     <li><a href="adventure.php">Adventure</a></li>
     <li><a href="puzzle.php">Puzzle</a></li>
 
 </ul>
+<center>
+<table cellspacing="130" style="border-left: 1px;">
 
+    <tr><td><img src="http://cdn.playbuzz.com/cdn/c7091d7d-e172-4a2e-9e95-d8e12e466d68/e3f86d85-04e5-41b2-886d-c7938409f467.jpg" style="width: 200px;height: 200px;"><h5>Game 1</h5></td>
+        <td><img src="http://fydo.net/wp-content/uploads/snake-screen2.png" style="width: 200px;height: 200px;"><h5>Game 2</h5></td>
+        <td><img src="https://i.ytimg.com/vi/v57gNCwnjRY/maxresdefault.jpg" style="width: 200px;height: 200px;"><h5>Game 3</h5></td>
+    </tr>
+    <tr>
+        <td><img src="https://upload.wikimedia.org/wikipedia/en/8/8c/A7800_Galaga.png" style="width: 200px;height: 200px;"><h5>Game 4</h5></td>
+        <td><img src="http://vignette4.wikia.nocookie.net/doom/images/2/21/E1M1_imps.png/revision/latest?cb=20090215170556" style="width: 200px;height: 200px;"><h5>Game 5</h5></td>
+        <td><img src="http://cinemassacre.com/wp-content/uploads/2015/10/mkfight.jpg" style="width: 200px;height: 200px;"><h5>Game 6</h5></td>
+    </tr>
+    <tr>
+        <td><img src="http://3.bp.blogspot.com/-CU9MWwcL52k/TrKMSlVJymI/AAAAAAAAALc/GEN3JHAwJSI/s1600/Old+School+Game+Ban+in+Android+Market+_img_1.png" style="width: 200px;height: 200px;"><h5>Game 7</h5></td>
+        <td><img src="https://upload.wikimedia.org/wikipedia/en/f/fd/Donkey_Kong_Gameplay.png" style="width: 200px;height: 200px;"><h5>Game 8</h5></td>
+        <td><img src="http://s3.amazonaws.com/mpatric/assets/original/snake-screenshot.png" style="width: 200px;height: 200px;"><h5>Game 9</h5></td>
+    </tr>
+</table></center>
 </body>
 </html>
->>>>>>> a2527291316bb88855161c6012b6ef4f2f319611
+
