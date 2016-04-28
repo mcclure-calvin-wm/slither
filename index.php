862cf961c75a4c9f7f42c9a049752068b4a9d90d
Rogelio Garcia <rogelio.garcia@west-mec.org>
2016-04-28 14:09:31 -0700

    Added opacity on home page

--- index.php
+++ index.php
@@ -13,32 +13,32 @@
     <li id="li3"><a href="puzzle.php">Puzzle</a></li>
 
 </ul>
+
 </div>
 
 <div id="div2">
     <ul>
         <li id="li4"><a href="index.php"><img src="images/home-icon.png" width="50" height="50"> </a></li>
         <li id="li5"><a href="adventure.php"><img src="images/settings.png" width="50" height="50"></a></li>
-        <li id="li6"><a href="puzzle.php"><img src="images/prof.png" width="50" height="50"></a></li>
+        <li id="li6"><a href="adventure.php"><img src="images/prof.png" width="50" height="50" ></a></div>
+</ul>
 
-    </ul>
-</div>
 <center>
     <table cellspacing="130" style="border-left: 1px;">
 
-        <tr><td><img src="http://cdn.playbuzz.com/cdn/c7091d7d-e172-4a2e-9e95-d8e12e466d68/e3f86d85-04e5-41b2-886d-c7938409f467.jpg" style="width: 200px;height: 200px;"><h5>Game 1</h5></td>
-            <td><img src="http://fydo.net/wp-content/uploads/snake-screen2.png" style="width: 200px;height: 200px;"><h5>Game 2</h5></td>
-            <td><img src="https://i.ytimg.com/vi/v57gNCwnjRY/maxresdefault.jpg" style="width: 200px;height: 200px;"><h5>Game 3</h5></td>
+        <tr> <td><div class="gamehover"><img src="http://cdn.playbuzz.com/cdn/c7091d7d-e172-4a2e-9e95-d8e12e466d68/e3f86d85-04e5-41b2-886d-c7938409f467.jpg" style="width: 200px;height: 200px;"></div><h5>Game 1</h5></td>
+            <td><div class="gamehover"><img src="http://fydo.net/wp-content/uploads/snake-screen2.png" style="width: 200px;height: 200px;"></div><h5>Game 2</h5></td>
+            <td><div class="gamehover"><img src="https://i.ytimg.com/vi/v57gNCwnjRY/maxresdefault.jpg" style="width: 200px;height: 200px;"></div><h5>Game 3</h5></td>
         </tr>
         <tr>
-            <td><img src="https://upload.wikimedia.org/wikipedia/en/8/8c/A7800_Galaga.png" style="width: 200px;height: 200px;"><h5>Game 4</h5></td>
-            <td><img src="http://vignette4.wikia.nocookie.net/doom/images/2/21/E1M1_imps.png/revision/latest?cb=20090215170556" style="width: 200px;height: 200px;"><h5>Game 5</h5></td>
-            <td><img src="http://cinemassacre.com/wp-content/uploads/2015/10/mkfight.jpg" style="width: 200px;height: 200px;"><h5>Game 6</h5></td>
+            <td><div class="gamehover"><img src="https://upload.wikimedia.org/wikipedia/en/8/8c/A7800_Galaga.png" style="width: 200px;height: 200px;"></div><h5>Game 4</h5></td>
+            <td><div class="gamehover"><img src="http://vignette4.wikia.nocookie.net/doom/images/2/21/E1M1_imps.png/revision/latest?cb=20090215170556" style="width: 200px;height: 200px;"></div><h5>Game 5</h5></td>
+            <td><div class="gamehover"><img src="http://cinemassacre.com/wp-content/uploads/2015/10/mkfight.jpg" style="width: 200px;height: 200px;"></div><h5>Game 6</h5></td>
         </tr>
         <tr>
-            <td><img src="http://3.bp.blogspot.com/-CU9MWwcL52k/TrKMSlVJymI/AAAAAAAAALc/GEN3JHAwJSI/s1600/Old+School+Game+Ban+in+Android+Market+_img_1.png" style="width: 200px;height: 200px;"><h5>Game 7</h5></td>
-            <td><img src="https://upload.wikimedia.org/wikipedia/en/f/fd/Donkey_Kong_Gameplay.png" style="width: 200px;height: 200px;"><h5>Game 8</h5></td>
-            <td><img src="http://s3.amazonaws.com/mpatric/assets/original/snake-screenshot.png" style="width: 200px;height: 200px;"><h5>Game 9</h5></td>
+            <td><div class="gamehover"><img src="http://3.bp.blogspot.com/-CU9MWwcL52k/TrKMSlVJymI/AAAAAAAAALc/GEN3JHAwJSI/s1600/Old+School+Game+Ban+in+Android+Market+_img_1.png" style="width: 200px;height: 200px;"></div><h5>Game 7</h5></td>
+            <td><div class="gamehover"><img src="https://upload.wikimedia.org/wikipedia/en/f/fd/Donkey_Kong_Gameplay.png" style="width: 200px;height: 200px;"></div><h5>Game 8</h5></td>
+            <td><div class="gamehover"><img src="http://s3.amazonaws.com/mpatric/assets/original/snake-screenshot.png" style="width: 200px;height: 200px;"></div><h5>Game 9</h5></td>
         </tr>
     </table></center>
 
