1069a34d65ff61d84b6cb63d7072039c4d8e08d6
Rogelio Garcia <rogelio.garcia@west-mec.org>
2016-04-28 14:46:14 -0700

    Added sign in and Create account(With inputs)

---
+++ createAccount.php
@@ -0,0 +1,50 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+    <link rel="stylesheet" type="text/css" href="styles.css">
+
+</head>
+<body>
+<div id="div1">
+    <ul>
+        <li id="li1"><a href="action.php">Action</a></li>
+        <li id="li2"><a href="adventure.php">Adventure</a></li>
+        <li id="li3"><a href="puzzle.php">Puzzle</a></li>
+
+    </ul>
+
+</div>
+
+<div id="div2">
+    <ul>
+        <li id="li4"><a href="index.php"><img src="images/home-icon.png" width="50" height="50"> </a></li>
+        <li id="li5"><a href="adventure.php"><img src="images/settings.png" width="50" height="50"></a></li>
+        <li id="li6"><a href="adventure.php"><img src="images/prof.png" width="50" height="50" ></a></div>
+</ul>
+
+
+
+<form>
+<p>First name:</p>
+    <input type="text">
+
+    <p>Last name:</p>
+    <input type="text">
+
+    <p>User name</p>
+    <input type="text">
+
+    <p>E-mail:</p>
+    <input type="email">
+
+    <p>Password:</p>
+    <input type="text">
+
+    <button type="submit">Create account</button>
+</form>
+
+</body>
+</html>
