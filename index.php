d2161ca1ac02afb1e86e4e5822e8114588fa4b1e
Calvin McClure <session1@START-Coding-MBP-16.local>
2016-05-02 14:31:40 -0700

    fixed the shadow on the logo

--- index.php
+++ index.php
@@ -7,25 +7,7 @@
         <link href="nav.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<div id="contain">
-    <div id="div1">
-        <ul>
-            <li id="li1" class="li"><a href="action.php">Action</a></li>
-            <li id="li2" class="li"><a href="adventure.php">Adventure</a></li>
-            <li id="li3" class="li"><a href="puzzle.php">Puzzle</a></li>
-            <li id="li4" class="li"><a href="strategy.php">Strategy</a></li>
-            <li id="li5"><a href="card.php">Card Games</a></li>
-
-        </ul>
-    </div>
-        <img src="images/slither-logo.png" id="logo">
-        <ul id="icon-nav">
-            <li id="li4"><a href="index.php"><img src="images/home-icon.png" width="50" height="50"> </a></li>
-            <li id="li5"><a href="adventure.php"><img src="images/settings.png" width="50" height="50"></a></li>
-            <li id="li6"><a href="puzzle.php"><img src="images/prof.png" width="50" height="50"></a></li>
-
-        </ul>
-</div>
+<?php require_once ("nav.php") ?>
 <div id="center-table">
     <table cellspacing="130" style="border-left: 1px;">
 
