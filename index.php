6792564a02620d4cd9c3b9adf3c260fc243fa4b9
Calvin McClure <session1@START-Coding-MBP-16.local>
2016-05-02 14:42:42 -0700

    fixed formatting

--- index.php
+++ index.php
@@ -11,19 +11,30 @@
 <div id="center-table">
     <table cellspacing="130" style="border-left: 1px;">
 
-        <tr><td><img class="gameop" src="images/btd5.jpeg" style="width: 200px;height: 200px;"><h5>BlOONS TD5</h5></td>
-            <td><img class="gameop" src="http://fydo.net/wp-content/uploads/snake-screen2.png" style="width: 200px;height: 200px;"><h5>Game 2</h5></td>
-            <td><img class="gameop" src="https://i.ytimg.com/vi/v57gNCwnjRY/maxresdefault.jpg" style="width: 200px;height: 200px;"><h5>Game 3</h5></td>
+        <tr>
+            <td><img class="gameop" src="images/btd5.jpeg" style="width: 200px;height: 200px;"><h5>BlOONS TD5</h5></td>
+            <td><img class="gameop" src="http://fydo.net/wp-content/uploads/snake-screen2.png"
+                     style="width: 200px;height: 200px;"><h5>Game 2</h5></td>
+            <td><img class="gameop" src="https://i.ytimg.com/vi/v57gNCwnjRY/maxresdefault.jpg"
+                     style="width: 200px;height: 200px;"><h5>Game 3</h5></td>
         </tr>
         <tr>
-            <td><img class="gameop" src="https://upload.wikimedia.org/wikipedia/en/8/8c/A7800_Galaga.png" style="width: 200px;height: 200px;"><h5>Game 4</h5></td>
-            <td><img class="gameop" src="http://vignette4.wikia.nocookie.net/doom/images/2/21/E1M1_imps.png/revision/latest?cb=20090215170556" style="width: 200px;height: 200px;"><h5>Game 5</h5></td>
-            <td><img class="gameop" src="http://cinemassacre.com/wp-content/uploads/2015/10/mkfight.jpg" style="width: 200px;height: 200px;"><h5>Game 6</h5></td>
+            <td><img class="gameop" src="https://upload.wikimedia.org/wikipedia/en/8/8c/A7800_Galaga.png"
+                     style="width: 200px;height: 200px;"><h5>Game 4</h5></td>
+            <td><img class="gameop"
+                     src="http://vignette4.wikia.nocookie.net/doom/images/2/21/E1M1_imps.png/revision/latest?cb=20090215170556"
+                     style="width: 200px;height: 200px;"><h5>Game 5</h5></td>
+            <td><img class="gameop" src="http://cinemassacre.com/wp-content/uploads/2015/10/mkfight.jpg"
+                     style="width: 200px;height: 200px;"><h5>Game 6</h5></td>
         </tr>
         <tr>
-            <td><img class="gameop" src="http://3.bp.blogspot.com/-CU9MWwcL52k/TrKMSlVJymI/AAAAAAAAALc/GEN3JHAwJSI/s1600/Old+School+Game+Ban+in+Android+Market+_img_1.png" style="width: 200px;height: 200px;"><h5>Game 7</h5></td>
-            <td><img class="gameop" src="https://upload.wikimedia.org/wikipedia/en/f/fd/Donkey_Kong_Gameplay.png" style="width: 200px;height: 200px;"><h5>Game 8</h5></td>
-            <td><img class="gameop" src="http://s3.amazonaws.com/mpatric/assets/original/snake-screenshot.png" style="width: 200px;height: 200px;"><h5>Game 9</h5></td>
+            <td><img class="gameop"
+                     src="http://3.bp.blogspot.com/-CU9MWwcL52k/TrKMSlVJymI/AAAAAAAAALc/GEN3JHAwJSI/s1600/Old+School+Game+Ban+in+Android+Market+_img_1.png"
+                     style="width: 200px;height: 200px;"><h5>Game 7</h5></td>
+            <td><img class="gameop" src="https://upload.wikimedia.org/wikipedia/en/f/fd/Donkey_Kong_Gameplay.png"
+                     style="width: 200px;height: 200px;"><h5>Game 8</h5></td>
+            <td><img class="gameop" src="http://s3.amazonaws.com/mpatric/assets/original/snake-screenshot.png"
+                     style="width: 200px;height: 200px;"><h5>Game 9</h5></td>
         </tr>
     </table>
 </div>
