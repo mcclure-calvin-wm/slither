152c89a9e1828f0e82f5856ff705fa94a368f3bf
Calvin McClure <session1@START-Coding-MBP-16.local>
2016-05-17 15:07:21 -0700

    casl

--- card.php
+++ card.php
@@ -1,7 +1,18 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: session1
- * Date: 5/17/16
- * Time: 3:01 PM
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="icon" href="images/slither-logo.png">
+    <title>Slither</title>
+    <?php require_once("links.php"); ?>
+
+</head>
+<body>
+<?php require_once ("nav.php") ?>
+<div id="center-table">
+    <h1 id="sorry">More Card Games On The Way!</h1>
+</div>
+
+
+
+</body>
+</html>
