7abb40255e17cc7ab556266ffc8ceea0c828234e
Calvin McClure <session1@START-Coding-MBP-16.local>
2016-05-17 13:51:33 -0700

    jjjjjj

---
+++ arcade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="icon" href="images/slither-logo.png">
+    <title>Slither</title>
+    <?php require_once("links.php"); ?>
+
+</head>
+<body>
+<?php require_once ("nav.php") ?>
+<div id="center-table">
+    <table cellspacing="130" style="border-left: 1px;">
+        <tr>
+            <td><a href="game.php?id=1"><img class="gameop" src="images/Apple.png"
+                                             style="width: 200px;height: 200px;"></a><h5>Apple Shooter</h5></td>
+            <td><a href="game.php?id=2"><img class="gameop" src="images/qwop.png"
+                                             style="width: 200px;height: 200px;"></a><h5>QWOP</h5></td>
+            <td><a href="game.php?id=4"><img class="gameop" src="images/Cat%20Mario.png"
+                                             style="width: 200px;height: 200px;"></a><h5>Cat Mario</h5></td>
+        </tr>
+        <tr>
+            <td><a href="game.php?id=6"><img class="gameop" src="images/pac%20man.png"
+                                             style="width: 200px;height: 200px;"></a><h5>Pac Man</h5></td>
+            <td><a href="game.php?id=7"><img class="gameop"
+                                             src="images/Gun%20blood.png"
+                                             style="width: 200px;height: 200px;"></a><h5>Gun Blood</h5></td>
+            <td><a href="game.php?id=8"><img class="gameop" src="images/stick.jpg"
+                                             style="width: 200px;height: 200px;"></a><h5>Stick Wars</h5></td>
+        </tr>
+        <tr>
+
+            <td><a href="game.php?id=9"><img class="gameop" src="images/pigs.png"
+                                             style="width: 200px;height: 200px;"></a><h5>Bad Piggies</h5></td>
+        </tr>
+    </table>
+</div>
+
+
+
+</body>
+</html>
