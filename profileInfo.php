21fb9ccb1b6b086269df51caeb0c10f419c3da7a
Calvin McClure <session1@START-Coding-MBP-16.local>
2016-05-17 13:59:35 -0700

    mmmm

--- profileInfo.php
+++ profileInfo.php
@@ -12,18 +12,18 @@ require('connectvars.php');
 <body>
 <?php require_once ("nav.php") ?>
 <div style="position: relative; top: 12em; left: 5em">
-<<<<<<< HEAD
-<table>
-    <tr><td><h3>Username:</h3><h1><?php echo $_SESSION['userName'] ?></h1></td></tr>
-=======
+
+<!--<table>-->
+<!--    <tr><td><h3>Username:</h3><h1>--><?php //echo $_SESSION['userName'] ?><!--</h1></td></tr>-->
+
 <table >
     <?php if(isset($_SESSION['password'])){
         echo "<tr><td><h3>Username: </h3><h1>" . $_SESSION["userName"] . "</h1></td></tr>";
     }else{
-        echo "<h1 id='notsign'>Not signed in</h1>";
+        echo "<h1 id='notsign' style='position: absolute; top: .1em; left: 2.5em; font-size: 5em;'>Please sign in to view this page</h1>";
     }
     ?>
->>>>>>> Rogelio
+
 
 </table>
 
